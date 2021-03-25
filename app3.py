c8b03328469f3c397926cfeb19d9b168729cf040
marilia reis <mariliabreis@gmail.com>
2021-03-25 15:43:18 -0300

    navigation bar working

--- app3.py
+++ app3.py
@@ -10,22 +10,27 @@ def get_unique_ingredientes():
     return pd.read_csv('data/ordered_keys.csv', compression='infer', storage_options=None)
 ORDERED_KEYS = get_unique_ingredientes()
 
-   
-Ingredientes = st.sidebar.multiselect('Selecione seu ingrediente', ORDERED_KEYS['replaced'].unique())
+navigation = st.sidebar.radio('Navigation',['About','Playground'])
+if navigation == 'About':
+    st.write('About page here')
+    ### code of the about page
+if navigation == 'Playground':
+    st.write('Playground goes here')
 
-for i in Ingredientes:
-    print(i)
-    st.write(ORDERED_KEYS[ORDERED_KEYS['replaced']==i][['replaced','id']])
+    Ingredientes = st.sidebar.multiselect('Selecione seu ingrediente', ORDERED_KEYS['replaced'].unique())
+    for i in Ingredientes:
+        print(i)
+        st.write(ORDERED_KEYS[ORDERED_KEYS['replaced']==i][['replaced','id']])
 
-if st.sidebar.checkbox('Melhores combinações'):
-    st.subheader('Suas melhores combinações são:')
-    st.write('Best ingredientes match')
-if st.sidebar.checkbox('Ingredientes mais utilizados'):
-    st.subheader('Seus ingredienntes mais utilizados são:')
-    st.write('Sal')
-if st.sidebar.checkbox('Ingredientes menos utilizados'):
-    st.subheader('Seus ingredienntes menos utilizados são:')
-    st.write('Chuchu')
+    if st.sidebar.checkbox('Melhores combinações'):
+        st.subheader('Suas melhores combinações são:')
+        st.write('Best ingredientes match')
+    if st.sidebar.checkbox('Ingredientes mais utilizados'):
+        st.subheader('Seus ingredienntes mais utilizados são:')
+        st.write('Sal')
+    if st.sidebar.checkbox('Ingredientes menos utilizados'):
+        st.subheader('Seus ingredienntes menos utilizados são:')
+        st.write('Chuchu')
 
 
 st.sidebar.title('Mais informações:')
